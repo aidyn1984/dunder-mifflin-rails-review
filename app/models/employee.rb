cdd86f7e23a01f5d7a789f5a8503b8bf716c074d
aidyn1984 <71663287+aidyn1984@users.noreply.github.com>
2021-04-08 15:01:04 -0500

    The Dunder Mifflin Pet Show

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    # validates :alias, uniqueness: { scope: :year,
+    # message: "This alias is already taken!" }
+
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
 end
